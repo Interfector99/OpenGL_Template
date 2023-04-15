72932e508c2019cdce497f09af9bc529d0586689
Interfector99 <interfector99@gmail.com>
2023-04-16 00:43:32 +0200

    Add project files.

---
+++ main.cpp
@@ -0,0 +1,20 @@
+// include own modules
+#include "Window.hpp"
+
+int main()
+{
+	display::Window window = display::Window();
+	try
+	{
+		window.init("Template", false, 800, 800, 60);
+		window.render();
+		window.destroy();
+	}
+	catch (const char* e)
+	{
+		std::cerr << e << std::endl;
+		window.destroy();
+	}
+	
+	return 0;
+}
